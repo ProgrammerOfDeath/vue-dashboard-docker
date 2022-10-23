0faafc12d107e8a2a97308a03dd8356947ebe660
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-23 08:54:31 +0200

    view: create new view for dashboard page

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,17 @@
+<script></script>
+
+<template>
+  <main>
+    <h1>My Dashboard</h1>
+    <div>
+      <h2>Latest Posts</h2>
+      <div class="flex">
+        <AppCard>
+          <h3>Post</h3>
+        </AppCard>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style></style>
