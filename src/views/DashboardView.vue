ed7921a16539a36cfd9e2a11b8f86124e4858a87
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-23 15:44:00 +0200

    view: adjust dashboard template

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
-import AppCard from '../components/ui/AppCard.vue'
-import AppMain from '../components/layout/AppMain.vue'
+import AppMain from '@/components/layout/AppMain.vue'
+import TodoList from '@/components/TodoList.vue'
+import PostsList from '@/components/PostsList.vue'
 </script>
 
 <template>
   <AppMain>
-    <h1>My Dashboard</h1>
-    <div>
-      <h2>Latest Posts</h2>
-      <div class="flex">
-        <AppCard>
-          <h3>Post</h3>
-        </AppCard>
-      </div>
-    </div>
+    <h1 class="text-center text-2xl mb-6">My Dashboard</h1>
+    <section class="mb-10">
+      <h2 class="text-center text-xl mb-4">Latest Posts</h2>
+      <PostsList></PostsList>
+    </section>
+    <section>
+      <h2 class="text-center text-xl mb-4">My Todos</h2>
+      <TodoList></TodoList>
+    </section>
   </AppMain>
 </template>
 
