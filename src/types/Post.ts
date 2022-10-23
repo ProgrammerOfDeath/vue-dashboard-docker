11d1a0dc68a514be1c773096aa963b7c58fc9500
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-23 15:44:14 +0200

    types: add interfaces for todos and posts

---
+++ src/types/Post.ts
@@ -0,0 +1,6 @@
+export default interface Post {
+  id: string
+  user_id: number | string
+  title: string
+  body: string
+}
---
+++ src/types/Todo.ts
@@ -0,0 +1,7 @@
+export default interface Todo {
+  id: string
+  user_id: number
+  title: string
+  due_on: string
+  status: string
+}
