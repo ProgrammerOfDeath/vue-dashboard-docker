a90ef903a9ffea95d3823c189e91c05efb80076c
ProgrammerOfDeath <rene.hotho@gmail.com>
2022-10-23 10:08:40 +0200

    component: adjust card

--- src/components/ui/AppCard.vue
+++ src/components/ui/AppCard.vue
@@ -1,7 +1,7 @@
 <script></script>
 
 <template>
-  <div class="card bg-slate-500 text-white w-96 shadow-xl">
+  <div class="card bg-base-100 text-white w-96 shadow-xl">
     <slot></slot>
   </div>
 </template>
